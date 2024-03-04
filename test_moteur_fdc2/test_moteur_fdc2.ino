1039ecdd1cf06be3222849e50a17fd419d754bab
mathieu <math86340@orange.fr>
2024-03-04 17:00:12 +0100

    Update test_moteur_fdc2.ino

--- test_moteur_fdc2/test_moteur_fdc2.ino
+++ test_moteur_fdc2/test_moteur_fdc2.ino
@@ -1,12 +1,12 @@
 #include <AFMotor.h>
 
-  AF_DCMotor motor(2, MOTOR12_8KHZ); // Création du moteur n ° 2 , 8KHz pwm
+  AF_DCMotor motor(2, MOTOR12_1KHZ); // Création du moteur n ° 2 , 8KHz pwm
 
   int seuil = 300; //seuil luminosité modifiable ici ou avec la vis sur le capteur
   int captfdc1; //declaration de la variable 0 ou 1 soit ouvert ou fermée
   int captfdc2; //declaration de la variable 0 ou 1 soit ouvert ou fermée
 
-  const int VMotor = 90; // changer ce parametre permet de changer la vitesse du moteur
+  const int VMotor = 255; // changer ce parametre permet de changer la vitesse du moteur
 
   int captLum = A2; //declaration du pin du capteur
   int FDC1 = A3;    //declaration du pin du capteur
@@ -36,22 +36,22 @@ void loop() {
      motor.setSpeed(VMotor);
      motor.run(BACKWARD);    
   }
-  captfdc1 = digitalRead(capteurfdc1);
-  captfdc2 = digitalRead(capteurfdc2);
+  captfdc1 = digitalRead(FDC1);
+  captfdc2 = digitalRead(FDC2);
   
   if(captfdc1 ==0){
     Serial.print ("fermeture Soir \n");
     motor.setSpeed(VMotor);
     motor.run(FORWARD); // moteur en marche
   }
-  captfdc1 = digitalRead(capteurfdc1);
-  captfdc2 = digitalRead(capteurfdc2);
+  captfdc1 = digitalRead(FDC1);
+  captfdc2 = digitalRead(FDC2);
 
   if(captfdc1 ==1){
     motor.run(RELEASE);
   }
-  captfdc1 = digitalRead(capteurfdc1);
-  captfdc2 = digitalRead(capteurfdc2);
+  captfdc1 = digitalRead(FDC1);
+  captfdc2 = digitalRead(FDC2);
   
   if(captfdc1 ==0){
     motor.run(RELEASE);
